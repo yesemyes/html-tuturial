19f1df60d99bafdf0e4b7617a0faff6e100e4168
yesemyes <yesemyes1@gmail.com>
2014-01-18 03:56:29 -0800

    Update Dispatcher.php

--- Dispatcher.php
+++ Dispatcher.php
@@ -1,1 +1,16 @@
-Dispatcher
+<?php 
+
+	if(isset($_GET['module']) && ($_GET['module'] == 'films' || $_GET['module'] == 'film'))
+		include('pages/films.php');
+	elseif(isset($_GET['module']) && ($_GET['module'] == 'actors' || $_GET['module'] == 'actor'))
+		include('pages/actors.php');
+	elseif(isset($_GET['module']) && $_GET['module'] == 'search')
+		include('pages/search.php');
+	elseif (isset($_GET['module']) && $_GET['module'] == '')
+		include ('pages/films.php');
+	else
+		include('pages/404.php');
+
+
+
+?>
