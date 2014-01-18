79b4ecd20f43c4621a54bccc30e7c7308bc01710
yesemyes <yesemyes1@gmail.com>
2014-01-18 03:55:53 -0800

    Update Config.php

--- Config.php
+++ Config.php
@@ -1,1 +1,23 @@
-Hello config
+<?php
+return array(
+	/**
+	*Database Configuration
+	*@var type is driver type
+	*@var params is datebase connection params
+	*/
+	'database' => array(
+		'type' => 'mysql',
+		'params' => array(
+			'dbName' => 'tank_1',
+			'dbUser' => 'root',
+			'dbPassword' => '',
+			'dbCharset' => 'utf8',
+			'dbHost' => 'localhost'),
+	),
+	/*
+	*Debug mode
+	*@var 1 is enabled, 0 is disables
+	*/
+	'debug' => 1,
+);
+?>
