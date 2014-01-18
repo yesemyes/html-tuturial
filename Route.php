8e94ff1544194bf1dda32510b72b92937cd437cd
yesemyes <yesemyes1@gmail.com>
2014-01-18 03:54:47 -0800

    Update Route.php

--- Route.php
+++ Route.php
@@ -1,1 +1,25 @@
-Route.php
+<?php 
+
+	$url = $_SERVER['REQUEST_URI'];
+
+	$urlParams = explode('/', $url);
+	array_shift($urlParams);
+	array_shift($urlParams);
+	array_shift($urlParams);
+	
+	foreach($urlParams as $key => $value){
+		if($key == 0)
+			$_GET['module'] = $value;
+		elseif($key == 1)
+			$_GET['id'] = $value;
+		else 
+			$_GET[$key] = $value;
+	}
+	
+
+sd
+
+
+
+
+?>
